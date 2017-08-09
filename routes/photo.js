5ec939d97cb0eb1c1b9a70f8b9c4a04c55db724c
= <houxing2846@163.com>
2017-08-09 18:02:10 +0800

    photo

--- routes/photo.js
+++ routes/photo.js
@@ -2,9 +2,13 @@
  * Created by houenxing on 17/7/21.
  */
 "use strict"
+const express = require('express');
+const router = express.Router();
+const path = require("path");
+const mime = require("mime");
+const fs = require("fs");
 
-var express = require('express');
-var router = express.Router();
+var image_mime = ["jpeg", "gif", "bmp"];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -19,7 +23,51 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function (req, res, next) {
+    let user = req.body.username;
+    console.log(user);
+    if(!!user) {
+        let imgList = []
 
+        let dir_path = path.join(__dirname, "../upload", user);
+        // console.log(dir_path);
+
+        let dirs = fs.readdirSync(dir_path);
+        // console.log("dir:" + dirs);
+
+        let src = "";
+
+        dirs.forEach(dir => {
+            // console.log(dir);
+            let temp = path.join(dir_path, dir);
+            // console.log(temp);
+            let stat = fs.statSync(temp);
+            // console.log(stat);
+            // console.log(stat.isDirectory());
+            // console.log(stat.isFile());
+
+            if(stat.isFile()) {
+                let type = mime.lookup(temp);
+                console.log(type);
+                let array = type.split('/');
+
+                if(array[0] === "image") {
+                    if(image_mime.indexOf(array[1]) > -1 ){
+                        console.log("Image file!");
+                        imgList.push(temp);
+                    }
+                }
+                // if()
+            }
+            else {
+                console.log("directory");
+            }
+        })
+        res.send(imgList);
+        // res.render('photo', {user: user, src: src});
+    }
+    else {
+        res.send('no user');
+    }
 });
 
 module.exports = router;
